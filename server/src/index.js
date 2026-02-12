e9a2b3f1de9e51c4b0838f91175157f825153847
Taylor <taylor@cultivatedcocktails.com>
2026-02-12 15:51:53 -0500

    Fix Express 5 catch-all route syntax for production

--- server/src/index.js
+++ server/src/index.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../public')));
 
     // The catch-all route: for any request that doesn't match an API route, send back index.html
-    app.get('*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, '../public', 'index.html'));
     });
 }
